ed2d30cd1daddd9b3b222d689bf199aaeb25aaf8
dcahoon <dylancahoon651@gmail.com>
2022-04-26 23:45:36 -0400

    new push

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -64,7 +64,6 @@ function Dashboard({ date }) {
 		setTrigger((prev) => !prev)
 	}
 	
-	
 	const reservationsContent = reservations.map((reservation, index) => (		
 		<tr key={index}>
 			<td>{reservation.first_name}</td>
@@ -157,32 +156,3 @@ function Dashboard({ date }) {
 }
 
 export default Dashboard;
-
-
-/* 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
- */
